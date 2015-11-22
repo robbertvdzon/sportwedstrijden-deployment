fab938da3fa2981664add54b5e3afcca09582ce4
Robbert <robbert@vdzon.com>
2015-11-22 19:12:33 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# sportwedstrijden-deployment
+#sportwedstrijden-deployment
+
+To run this at boot time:
+```
+echo "@reboot root /workspace/sportwedstrijden-deployment/download_and_start.sh &" > /etc/cron.d/msw-portlet
+```
--- download_and_start.sh
+++ download_and_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl http://www.vdzon.com/_jar/msw-portlet.php --output portlet_data/mijnsportwedstrijden.jar
+curl http://www.vdzon.com/_jar/msw-portlet.php --output /workspace/sportwedstrijden-deployment/portlet_data/mijnsportwedstrijden.jar
 docker-compose build
 docker-compose up -d
