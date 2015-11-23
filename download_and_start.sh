a8e17d516aea4c533e07a3bb5e52021f4851e5cd
robbertvdzon <robbert@vdzon.com>
2015-11-23 09:26:52 +0100

    Update download_and_start.sh

--- download_and_start.sh
+++ download_and_start.sh
@@ -4,5 +4,6 @@ echo Updating and starting mijnsportwedstrijden
 service docker start
 cd /workspace/sportwedstrijden-deployment/
 curl http://www.vdzon.com/_jar/msw-portlet.php --output portlet_data/mijnsportwedstrijden.jar
+sleep 10
 docker-compose build
 docker-compose up -d
