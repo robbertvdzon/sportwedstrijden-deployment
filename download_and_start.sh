4a4630942b56319868f295f498dbac3478e81936
Robbert <robbert@vdzon.com>
2015-11-27 11:55:50 +0100

    download all 4 jar files

--- download_and_start.sh
+++ download_and_start.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+set -e
 exec 1> >(logger -s -t $(basename $0)) 2>&1
 echo Updating and starting mijnsportwedstrijden
 cd /workspace/sportwedstrijden-deployment/
-curl http://www.vdzon.com/_jar/msw-portlet.php --output portlet_data/mijnsportwedstrijden.jar
+curl http://www.vdzon.com/_jar/download.php?file=edgeserver.jar --output data/edgeserver_data/edgeserver.jar
+curl http://www.vdzon.com/_jar/download.php?file=storageserver.jar --output data/storageserver_data/storageserver.jar
+curl http://www.vdzon.com/_jar/download.php?file=emailservice.jar --output data/emailservice_data/emailservice.jar
+curl http://www.vdzon.com/_jar/download.php?file=eventservice.jar --output data/eventservice_data/eventservice.jar
+
 sleep 10
 /usr/local/bin/docker-compose build
 /usr/local/bin/docker-compose up -d
