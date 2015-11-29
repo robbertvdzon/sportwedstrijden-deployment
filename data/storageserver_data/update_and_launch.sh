f966bed1f0765c8e49128d6aac56f42edf22c97d
Robbert <robbert@vdzon.com>
2015-11-29 08:17:42 +0100

    fixes

--- data/storageserver_data/update_and_launch.sh
+++ data/storageserver_data/update_and_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mvn flyway:migrate
+#mvn flyway:migrate
 java -Djava.security.egd=file:/dev/./urandom -jar /storageserver_data/storageserver.jar --spring.config.location=application.properties
